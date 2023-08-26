2e880339c8aff48b31def4cfbfd2cbed3ceccbe2
suk-18 <77617146+suk-18@users.noreply.github.com>
2023-08-26 22:59:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var Color = [
+    "pink",
+    "yellow",
+    "black",
+    "voilet",
+    "blue",
+    "green",
+    "purple",
+    "orange",
+    "lightgreen",
+    "lemon",
+  ];
+  var index = 0 ;
+  var div = document.querySelector("div");
+  const button = document.querySelector("button");
+  button.addEventListener("click", function() {
+      if (index === Color.length) {
+          index = 0;
+    }
+          div.style.backgroundColor = Color[index];
+          index++;
+  })
